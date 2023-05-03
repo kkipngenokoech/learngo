8085c5f52d0ff111691785d03debc80a5ebf6d5f
kip <kkipngenokoech22@gmail.com>
2023-05-03 15:13:02 +0300

    i know a little about routes

--- 02.HelloMultiverseServer/hellomultiverse.go
+++ 02.HelloMultiverseServer/hellomultiverse.go
@@ -11,9 +11,14 @@ func main(){
 		fmt.Fprintf(w, "Hello and welcome to multiverse, you request for: %s\n", r.URL.Path)
 	})
 	http.HandleFunc("/show", Show)
+	http.HandleFunc("/captain", Captain)
 	http.ListenAndServe(":8080",nil)
 }
 
 func Show(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Hi there! let me show you how amazing you are because you requested for: %s\n", r.URL.Path)
+}
+
+func Captain(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Hi there captain, welcome to the deck")
 }
